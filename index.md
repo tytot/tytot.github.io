033b00039bb36670f50d2e0dc04f7bd1717cc3d4
Tyler Lin <tylerhlin@gmail.com>
2020-05-29 18:10:48 -0400

    Add litty site

--- index.md
+++
@@ -1,5 +0,0 @@
-# Games
-## Bubble Fishing
-[Privacy Policy](./bubble-fishing/privacy-policy.html)
-
-[Terms and Conditions](./bubble-fishing/terms-of-service.html)
---
+++ portfolio.js
@@ -0,0 +1,48 @@
+let entries = [
+    {
+        "name": "Bubble Fishing",
+        "description": "An almost-limitless arcade game made in Unity 3D for iOS. Players control a physics-defying bubble with the goal of sinking as deep in the ocean as possible. The game includes 15 different sea creatures, power-ups, achievements, and leaderboards. Scripted using C#.",
+        "links": {
+            "github": "https://github.com/tytot/bubble-fishing-src",
+            "link": "https://apps.apple.com/us/app/bubble-fishing/id1496553186?ls=1"
+        },
+        "tags": "C#, Unity, iOS, arcade, indie",
+        "image": "bubble-fishing.png"
+    },
+    {
+        "name": "Pool",
+        "description": "A fully-functional pool simulation made in Java Swing. Game modes include 8-ball, 9-ball, and 10-ball. The game features score-keeping, accurate collision detection and physics, and aim guidance. Inspiration for certain aspects came from Game Pigeon's 8-ball.",
+        "links": {
+            "github": "https://github.com/tytot/pool"
+        },
+        "tags": "Java, Swing, pool, physics",
+        "image": "pool.png"
+    },
+    {
+        "name": "Needle Exchange Program Mediator",
+        "description": "My internship project for the Johns Hopkins Applied Physics Lab. It connects various health services, including OpenHIM, OpenInfoMan, and RapidPro together to track the status of a needle exchange program. A custom-made human resource information system (HRIS) serves as the main point of interaction. The project aims to promote interoperability and efficent workflow.",
+        "links": {
+            "github": "https://github.com/tytot/needle-exchange"
+        },
+        "tags": "HTML, CSS, JavaScript, Bootstrap, async, interoperability, mediator",
+        "image": "nes.png"
+    },
+    {
+        "name": "tylerl.in",
+        "description": "This website! Made to improve my website creation skills and because the url is litty, it features an 'about' section, a portfolio, and an 'extras' section. Portfolio entries are dynamically generated using a local JSON file. There are a couple of hidden easter eggs; can you find them?",
+        "links": {
+            "github": "https://github.com/tytot/tytot.github.io"
+        },
+        "tags": "HTML, CSS, JavaScript, JQuery, website design",
+        "image": "website.png"
+    },
+    {
+        "name": "Chinese Checkers",
+        "description": "A faithful recreation of Chinese Checkers made from scratch in Java. Up to 6 players can play. The game features optional CPUs and variable screen sizes.",
+        "links": {
+            "github": "https://github.com/tytot/chinese-checkers"
+        },
+        "tags": "Java, chinese checkers",
+        "image": "chinese-checkers.png"
+    }
+]
