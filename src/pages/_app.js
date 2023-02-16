b34b61d896bd617692aeaecb692c81a951b4aa58
Tyler Lin <tylerlin@gatech.edu>
2023-02-16 15:15:23 -0500

    Implement dark mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
-import '@styles/global.scss'
+import { ThemeProvider } from 'next-themes'
 import { Layout } from '@components'
 import { mono, sans, serif } from '@styles/fonts'
 import { prefersReducedMotion } from '@utils'
+import '@styles/global.scss'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
@@ -25,18 +26,20 @@ export default function MyApp({ Component, pageProps }) {
     useNormalScrollRoutes()
 
     return (
-        <Layout>
-            <Component {...pageProps} />
-            <style jsx global>{`
-                :root {
-                    --font-sans: ${sans.style.fontFamily};
-                    --font-serif: ${serif.style.fontFamily};
-                    --font-mono: ${mono.style.fontFamily};
-                }
-                .reveal {
-                    visibility: ${prefersReducedMotion() ? 'visible' : 'hidden'};
-                }
-            `}</style>
-        </Layout>
+        <ThemeProvider>
+            <Layout>
+                <Component {...pageProps} />
+                <style jsx global>{`
+                    :root {
+                        --font-sans: ${sans.style.fontFamily};
+                        --font-serif: ${serif.style.fontFamily};
+                        --font-mono: ${mono.style.fontFamily};
+                    }
+                    .reveal {
+                        visibility: ${prefersReducedMotion() ? 'visible' : 'hidden'};
+                    }
+                `}</style>
+            </Layout>
+        </ThemeProvider>
     )
 }
