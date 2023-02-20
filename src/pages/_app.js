aedaf20af101c757cd68ae2f40948d161d32505d
Tyler Lin <tylerlin@gatech.edu>
2023-02-20 00:32:13 -0500

    Add favicon and export

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import { ThemeProvider } from 'next-themes'
 import { Layout } from '@components'
 import { mono, sans, serif } from '@styles/fonts'
